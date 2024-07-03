1f66a00917fe3610990e79953642daa619f580e4
nguyenhoangkhue <snh110685@gmail.com>
2024-07-03 18:42:30 +0700

    upd

--- src/main/java/com/example/javaspringbootb30/model/request/SignupRequest.java
+++ src/main/java/com/example/javaspringbootb30/model/request/SignupRequest.java
@@ -1,6 +1,10 @@
 package com.example.javaspringbootb30.model.request;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -9,7 +13,12 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SignupRequest {
+    @NotEmpty(message = "Tên người dùng không được để trống")
     String name;
+    @Column(unique = true,nullable = false)
+    @Email(message="Email không đúng định dạng")
     String email;
+    @NotEmpty(message = "Mật khẩu không được để trống")
+    @Length(min=3,message="Mật khẩu phải có ít nhất 3 kí tự")
     String password;
 }
