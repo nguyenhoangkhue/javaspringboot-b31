8a131a68ab94a6037e97760cccf8e90dbed11e1d
nguyenhoangkhue <snh110685@gmail.com>
2024-07-03 16:33:46 +0700

    upd

--- src/main/java/com/example/javaspringbootb30/controller/WebController.java
+++ src/main/java/com/example/javaspringbootb30/controller/WebController.java
@@ -1,8 +1,6 @@
 package com.example.javaspringbootb30.controller;
 
 import jakarta.annotation.security.RolesAllowed;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
